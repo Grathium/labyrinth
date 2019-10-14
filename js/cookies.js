fef1f776800c145721c6509c5e50a3092b0af5db
Grathium Industries <grathiumsoftwears@gmail.com>
2019-10-14 17:40:41 +1000

    Delete cookies.js

--- js/cookies.js
+++
@@ -1,8 +0,0 @@
-/*! See LICENSE for licensing information */
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = "ah" + "=" + "us-en%2Cau-en" + ";" + expires + ";path=/";
-}
