bf065136486289be64aba4f9cde94566cfacfcd4
Grathium Industries <grathiumsoftwears@gmail.com>
2020-01-09 14:40:27 +1000

    Delete script.js

--- MAIN/newtab/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('searchbar').onkeydown = function(e){
-    if (!e) e = window.event;
-    var keyCode = e.keyCode || e.which;
-    if (keyCode == '13'){
-      window.location.href = "https://duckduckgo.com/?q=" + document.getElementById('searchbar').value;
-      return false;
-    }
-  }
